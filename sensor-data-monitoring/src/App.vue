a9c2cd53f059dbc6b05f7073f1c9b96d13e991c3
minjeongkoo <koominjeong99@gmail.com>
2025-05-09 18:14:42 +0900

    기존 lidar-frontend 프로젝트 파일 삭제 및 sensor-data-monitoring 프로젝트 초기화, 기본 설정 파일 추가

---
+++ sensor-data-monitoring/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <router-view />
+</template>
+
+<style lang="scss">
+#app {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  color: #2c3e50;
+}
+</style>
